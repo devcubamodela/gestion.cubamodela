0e96c3b81da236eb418f7240e9d2d964b3d40bba
cergonzalezv <risolution9206@gmail.com>
2022-11-22 22:46:01 -0500

    Getting all products from Woocomerce

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -2,16 +2,18 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Products;
+use PhpParser\Node\Expr\Empty_;
+use Automattic\WooCommerce\Client;
+use PhpParser\Node\Expr\ArrayItem;
+use App\Repository\ProductsRepository;
+use App\Controller\GlobalFuntionsController;
+use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Controller\GlobalFuntionsController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Automattic\WooCommerce\Client;
-use App\Entity\Products;
-use App\Repository\ProductsRepository;
-
-use Symfony\Component\HttpClient\HttpClient;
+use Automattic\WooCommerce\HttpClient\HttpClientException;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 
 /**
@@ -20,101 +22,128 @@ use Symfony\Component\HttpClient\HttpClient;
 
 class ProductsController extends AbstractController
 {
-private $productsRepository;
+    private $productsRepository;
 
     public function __construct(ProductsRepository $productsRepository)
     {
         $this->productsRepository = $productsRepository;
-       
     }
-      /**
- * @Route("/all", name= "all_Products")
- */
-    public function auth(){
-        
+
+    public function auth()
+    {
+
         $woocommerce = new Client(
             'https://testingtiendaonline.cubamodela.com/',
             'ck_f5610087fad2e45d2450d04a3e5a4d508697a6e2',
             'cs_1793afc267a0ac84e1d55cbc764b33bfadf209ea',
             [
-                'wp_api'=> true,
+                'wp_api' => true,
                 'version' => 'wc/v3',
-                
             ]
         );
-       
+
         //var_dump($woocommerce->get('products'));die;
-      
-        $productos= $woocommerce->get('products');
+
+        $productos = $woocommerce->get('products', ['filter[limit]' => -1]);
 
         return $productos;
     }
 
-    public function productsVariation($id){
+    public function productsVariation($id)
+    {
 
         $woocommerce = new Client(
             'https://testingtiendaonline.cubamodela.com/',
             'ck_f5610087fad2e45d2450d04a3e5a4d508697a6e2',
             'cs_1793afc267a0ac84e1d55cbc764b33bfadf209ea',
             [
-                'wp_api'=> true,
+                'wp_api' => true,
                 'version' => 'wc/v3',
-                
+
             ]
         );
-       
+
         //var_dump($woocommerce->get('products'));die;
-       
-        $productos= $woocommerce->get('products/'.$id.'/variations');
-        
-    return $productos;
-}
 
 
-      /**
- * @Route("/all", name= "all_Products")
- */
-    public function all_Products(): JsonResponse
-    {
-        require __DIR__ . '/../../vendor/autoload.php';
-           $productos = $this->auth();
-          foreach($productos as $prod){
-            $id[]= [
-                "id"=>$prod->id,
-                "sku"=>$prod->sku,
-                "name"=>$prod->name,
-               /* "stock_quantity"=>$prod->stock_quantity,
-                "status"=>$prod->status,*/
-               
-           ];
-           $id[] = $this->productsVariation($prod->id);
-           
-           }
-               return new JsonResponse($id);
+        $productos = $woocommerce->get('products/' . $id . '/variations');
 
+        return $productos;
     }
 
-     /**
- * @Route("/push_products", name= "push_products")
- */
-public function push_products(): JsonResponse
-{
-    require __DIR__ . '/../../vendor/autoload.php';
-       $productos = $this->auth();
-       foreach($productos as $prod){
-        $id[]= [
-            $sku=$prod->sku,
-            $name=$prod->name,
-            $picture="",
-            $amount=$prod->stock_quantity,
-            $brand=$prod->slug,
-            $date=$prod->date_created,
-           
-        ];
-        $this->productsRepository->ProductRegister($name,$sku, $amount ,$picture, $brand, $date);
-           
-        }
-return new JsonResponse($id);
 
-}
+    /**
+     * @Route("/all", name= "allProducts")
+     */
+    public function AllProducts(): JsonResponse
+    {
+        $page = 1;
+        $products = [];
+        $all_products = [];
+        $woocommerce = new Client(
+            'https://testingtiendaonline.cubamodela.com/',
+            'ck_f5610087fad2e45d2450d04a3e5a4d508697a6e2',
+            'cs_1793afc267a0ac84e1d55cbc764b33bfadf209ea',
+            [
+                'wp_api' => true,
+                'version' => 'wc/v3',
+
+            ]
+        );
+
+        do {
+            try {
+                $products = $woocommerce->get('products', array('per_page' => 10, 'page' => $page));
+            } catch (HttpClientException $e) {
+                die("Can't get products: $e");
+            }
+            $all_products = array_merge($all_products, $products);
+            $page++;
+        } while (count($products) > 0);
+
+        foreach ($all_products as $prod) {
+            $id[] = [
+                "id" => $prod->id,
+                "sku" => $prod->sku,
+                "name" => $prod->name,
+                /* "stock_quantity"=>$prod->stock_quantity,
+                "status"=>$prod->status,*/
+
+            ];
+            $productsVariations = $this->productsVariation($prod->id);
+            foreach ($productsVariations as $prod1) {
+                $var1[] = [
+                    "id" => $prod1->id,
+                    "sku" => $prod1->sku,
+
+                ];
+            }
+            $id[] = $var1;
+        }
+
+        return new JsonResponse($id);
+    }
+
+
+    /**
+     * @Route("/push_products", name= "push_products")
+     */
+    public function push_products(): JsonResponse
+    {
+        require __DIR__ . '/../../vendor/autoload.php';
+        $productos = $this->auth();
+        foreach ($productos as $prod) {
+            $id[] = [
+                $sku = $prod->sku,
+                $name = $prod->name,
+                $picture = "",
+                $amount = $prod->stock_quantity,
+                $brand = $prod->slug,
+                $date = $prod->date_created,
+
+            ];
+            $this->productsRepository->ProductRegister($name, $sku, $amount, $picture, $brand, $date);
+        }
+        return new JsonResponse($id);
+    }
 }

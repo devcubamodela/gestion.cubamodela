1434cbb6ff2f29b345e37225bd3809e2b905939d
cergonzalezv <risolution9206@gmail.com>
2023-03-02 21:44:03 -0500

    Vista de Provaider adicionar estilos

--- src/Controller/GlobalFuntionsController.php
+++ src/Controller/GlobalFuntionsController.php
@@ -7,24 +7,35 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Automattic\WooCommerce\Client;
 use Symfony\Component\HttpFoundation\JsonResponse;
-
-    
-
-   
+use Symfony\Component\Validator\Constraints\Date;
 
 class GlobalFuntionsController extends AbstractController
 {
-    
-       /**
 
-    * @Route("", name="route_principal")
+    /**
 
-    */
+     * @Route("", name="route_principal")
+
+     */
     public function index()
     {
-      
+
         return $this->render('principal/principal.html.twig');
     }
+    // /**
+
+    //  * @Route("/time", name="route_principal")
+
+    //  */
+    // public function activarTrigger(): JsonResponse
+    // {
+    //     $hora = date('H:i:s');
+    //     if($hora>='14:20:00'){
+    //         return new JsonResponse("Super"); 
+    //     }
+    //     //Ejecutar tarea programada
+
+       
+    //     return new JsonResponse($hora);
+    // }
 }
-
-

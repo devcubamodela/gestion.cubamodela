9b6bae05074a687186520d959ce2022fa16a0586
cergonzalezv <risolution9206@gmail.com>
2023-03-13 21:12:33 -0400

    All

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -48,15 +48,15 @@ class RegistrationController extends AbstractController
             $entityManager->flush();
 
             // generate a signed url and email it to the user
-            // $this->emailVerifier->sendEmailConfirmation(
-            //     'app_verify_email',
-            //     $user,
-            //     (new TemplatedEmail())
-            //         ->from(new Address('mailer@cubamodela.com', 'Admin'))
-            //         ->to($user->getEmail())
-            //         ->subject('Please Confirm your Email')
-            //         ->htmlTemplate('registration/confirmation_email.html.twig')
-            // );
+            $this->emailVerifier->sendEmailConfirmation(
+                'app_verify_email',
+                $user,
+                (new TemplatedEmail())
+                    ->from(new Address('mailer@cubamodela.com', 'Admin'))
+                    ->to($user->getEmail())
+                    ->subject('Please Confirm your Email')
+                    ->htmlTemplate('registration/confirmation_email.html.twig')
+            );
             // do anything else you need here, like send an email
 
             return $userAuthenticator->authenticateUser(
